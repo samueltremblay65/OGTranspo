8af19cd5b1b325cd699d3c1db11d9b04a603bbb4
Samuel Tremblay <samuel.tremblay65@gmail.com>
2025-05-15 20:43:56 +0200

    Added code for destinations with location, radius and population

---
+++ Point.js
@@ -0,0 +1,18 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    distanceTo(point) {
+        let distance = Math.sqrt(Math.abs(this.x - point.x) ** 2 + Math.abs(this.y - point.y) ** 2);
+        return distance;
+    }
+
+    randomPointAround(radius) {
+        const x = this.x - radius + Math.round(Math.random() * 2 * radius);
+        const y = this.y - radius + Math.round(Math.random() * 2 * radius);
+
+        return new Point(x, y);
+    }
+}
