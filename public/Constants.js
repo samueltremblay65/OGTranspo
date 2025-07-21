56b71e90e097b2b5392a9adb4783278099909c51
Samuel Tremblay <samuel.tremblay65@gmail.com>
2025-07-21 14:23:00 +0200

    Important transit pathfinding algorithm bug fixes and improvements

--- public/Constants.js
+++ public/Constants.js
@@ -3,6 +3,7 @@ const M_PER_PIXEL = 8;
 const WALKING_SPEED = 1.2;
 const TRAIN_SPEED = 10;
 const STOP_TIME = 40;
+const TRANSFER_TIME = 60;
 
 const STARTING_BUDGET = 5000000;
 const COST_STATION = 5000;
